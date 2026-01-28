73a0d2ebf460170364b126e067170e0a2d749fab
yuin <dldmldlsy@naver.com>
2026-01-28 23:48:47 +0900

    repo init

---
+++ src/main/java/com/booking/service/fast_catch/FastCatchApplication.java
@@ -0,0 +1,13 @@
+package com.booking.service.fast_catch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCatchApplication.class, args);
+	}
+
+}

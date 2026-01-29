6220bff7aa93c2d81efd6b23f033214ee19de421
yuin <dldmldlsy@naver.com>
2026-01-30 00:02:57 +0900

    멀티 모듈 초기 세팅

---
+++ apps/api/src/main/java/com/booking/service/api/HelloController.java
@@ -0,0 +1,15 @@
+package com.booking.service.api;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "hello");
+    }
+}
